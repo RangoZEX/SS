7e61d345907bd332dc4a8d83793ba1125a934e64
MR. RANGO <74708667+RangoZEX@users.noreply.github.com>
2023-03-26 22:24:59 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,10 +6,10 @@ class Config:
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    SESSION_NAME = os.environ.get("SESSION_NAME", ":memory:")
+    SESSION_NAME = os.environ.get("SESSION_NAME", ":rango:")
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
     DATABASE_URL = os.environ.get("DATABASE_URL")
-    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")]
+    AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "1196263968").split(" ")]
     MAX_PROCESSES_PER_USER = int(os.environ.get("MAX_PROCESSES_PER_USER", 2))
     MAX_TRIM_DURATION = int(os.environ.get("MAX_TRIM_DURATION", 600))
     TRACK_CHANNEL = int(os.environ.get("TRACK_CHANNEL", False))
